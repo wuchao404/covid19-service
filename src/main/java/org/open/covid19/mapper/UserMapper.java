c548c7373155ce0f09cba1ff7be48b3504dde905
shiyuan2he <shiyuan4work@126.com>
2020-04-21 20:49:57 +0800

    build

--- src/main/java/org/open/covid19/mapper/UserMapper.java
+++ src/main/java/org/open/covid19/mapper/UserMapper.java
@@ -1,9 +1,9 @@
 package org.open.covid19.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.open.covid19.entity.UserEntity;
-import org.springframework.stereotype.Component;
 
-@Component
+@Mapper
 public interface UserMapper {
     UserEntity getUser(String username);
 }
